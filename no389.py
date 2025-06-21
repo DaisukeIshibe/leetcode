e70a33ad4b241b22d68ea09d5217fb37dc53624f
Daisuke Ishibe <daisuke.ishibe@gmail.com>
2025-06-21 10:44:50 +0900

    Update for no389

--- no389.py
+++ no389.py
@@ -1,29 +1,34 @@
+def makeDict(s: str) -> dict:
+	out_dict = {}
+	for c in s:
+		if not c in out_dict:
+			out_dict[c] = 1
+		else:
+			out_dict[c] += 1
+	return out_dict
+
+
 def findTheDifference(s: str, t: str) -> str:
-	if len(s) == 0:
-		return t
+	s_dict = makeDict(s)
+	t_dict = makeDict(t)
+	diff_list = list(set(t_dict.keys()) - set(s_dict.keys()))
+	if len(diff_list) > 0:
+		return diff_list[0]
 	
-	s_dict:dict = {}
-	for i in range(len(s)):
-		if s[i] in s_dict:
-			s_dict[s[i]] += 1
-		else:
-			s_dict[s[i]] = 1
+	t_list = sorted(t_dict.items(), key=lambda x:x[0])
+	s_list = sorted(s_dict.items(), key=lambda x:x[0])
 
-	t_dict:dict = {}
-	for i in range(len(t)):
-		if t[i] in t_dict:
-			t_dict[t[i]] += 1
-		else:
-			t_dict[t[i]] = 1
-	
-	for key_t in t_dict.keys():
-		if key_t in s_dict:
-			t_dict[key_t] -= 1
-
-	for k, v in t_dict.items():
-		if v >= 1:
-			return k
+	for e_t, e_s in zip(t_list, s_list):
+		t_count = e_t[1]
+		s_count = e_s[1]
+		if t_count != s_count:
+			return e_t[0]
 	return ''
 
 # Test cases
-print(findTheDifference("a", "aa"))  # Output: "a"
+print(findTheDifference("abcd", "abcde")) # Expect: "e"
+print(findTheDifference("", "y")) # Expect: "y"
+print(findTheDifference("a", "aa"))  # Expect: "a"
+a = "ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor"
+b = "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj"
+print(findTheDifference(a, b)) # Expect: "t"
