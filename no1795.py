919d85fa30a7043e35fb12811ea28a14c89ddbe2
Daisuke Ishibe <daisuke.ishibe@gmail.com>
2025-06-23 21:25:50 +0900

    Update for no1795

--- no1795.py
+++ no1795.py
@@ -17,42 +17,17 @@ import pandas as pd
 | 99         | 66     | null   | 286    |
 +------------+--------+--------+--------+
 '''
-#products_df = pd.DataFrame({
-#	'product_id': [0, 1],
-#	'store1': [95, 70],
-#	'store2': [100, None],
-#	'store3': [105, 80]
-#})
 products_df = pd.DataFrame({
-	'product_id': [38, 40, 67, 99],
-	'store1': [109, None, None, 66],
-	'store2': [742, None, 681, None],
-	'store3': [130, 604, 332, 286]
+	'product_id': [0, 1],
+	'store1': [95, 70],
+	'store2': [100, None],
+	'store3': [105, 80]
 })
+#products_df = pd.DataFrame({
+#	'product_id': [38, 40, 67, 99],
+###	'store3': [130, 604, 332, 286]
+#})
 
-products_t_df = products_df.T
-products_t_df = products_t_df.drop(products_t_df.index[0])
-columns = products_t_df.columns
-t_df = pd.concat([products_t_df[i] for i in columns], axis=1)
-t_df = t_df.reset_index()
-id_list = [[i]*len(products_t_df[i]) for i in columns.to_list()]
-id_list = [item for sublist in id_list for item in sublist]
-
-out_df = pd.DataFrame(columns=['product_id', 'store', 'price'])
-out_df['product_id'] = id_list
-store_list = [[s]*columns.size for s in t_df['index'].to_list()]
-store_list = [item for sublist in store_list for item in sublist]
-price_list = [t_df[i].to_list() for i in columns]
-price_list = [item for sublist in price_list for item in sublist]
-out_df['store'] = store_list
-out_df['price'] = price_list
-out_df = out_df.dropna()
-print(out_df)
-
-'''
-out_df.columns = ['store', 'price']
-out_df['product_id'] = id_list
-out_df = out_df[['product_id', 'store', 'price']]
-out_df = out_df.dropna()
-print(out_df)
-'''
+store_df = products_df.melt(id_vars=['product_id'], var_name='store', value_name='price')
+store_df = store_df.dropna(subset=['price']).sort_values(by='product_id')
+print(store_df)
