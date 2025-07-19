0a16edfcae0d1e2b91b1920d7687be3b6511881d
Daisuke Ishibe <daisuke.ishibe@gmail.com>
2025-07-19 22:27:52 +0900

    Update for no334

--- no334.py
+++ no334.py
@@ -1,32 +1,44 @@
-def increasingTriplet(nums: list[int]) -> bool:
-	if len(nums) < 3:
-		return False
+class Solution:
+    def increasingTriplet(self, nums: list[int]) -> bool:
+        len_n = len(nums)
+        if len_n < 3:
+            return False
+        if len_n == 3:
+            if nums[0] < nums[1] < nums[2]:
+                return True
+            else:
+                return False
+        
+        first = second = float('inf')
+        for n in nums:
+            if n < first:
+                first = n
+            elif n < second:
+                second = n
+            else:
+                return True
+        return False
 
-	nums_dict = {n:idx for idx, n in enumerate(nums)}
-	sorted_nums = sorted(nums)
-	len_nums = len(nums)
 
-	print(nums_dict)
-	for n in sorted_nums:
-		start_pos = nums_dict[n] + 1
-		for c in range(start_pos, len_nums):
-			if nums[c] > n:
-				for d in range(c + 1, len_nums):
-					if nums[d] > nums[c]:
-						return True
-
-	return False
-
+sol = Solution()
 nums0 = [1, 2, 3, 4, 5] # True
 nums1 = [5, 4, 3, 2, 1] # False
 nums2 = [2,1,5,0,4,6] # True
 nums3 = [20,100,10,12,5,13] # True
 nums4 = [6,7,1,2] # False
 nums5 = [1, 2, 1, 3] # True
-#print(f'{nums0} act = {increasingTriplet(nums0)} expect = True')
-#print(f'{nums1} act = {increasingTriplet(nums1)} expect = False')
-#print(f'{nums2} act = {increasingTriplet(nums2)} expect = True')
-#print(f'{nums3} act = {increasingTriplet(nums3)} expect = True')
-#print(f'{nums4} act = {increasingTriplet(nums4)} expect = False')
-print(f'{nums5} act = {increasingTriplet(nums5)} expect = True')
-# The above code is a brute force solution with O(n^2) time complexity.
+nums6 = [4,5,2147483647,1,2] # True
+nums7 = [0,4,2,1,0,-1,-3] # False
+nums8 = [2,1,5,0,3] # False
+nums9 = [1,5,0,4,1,3] # True
+
+print(f'{nums0} act = {sol.increasingTriplet(nums0)} expect = True')
+print(f'{nums1} act = {sol.increasingTriplet(nums1)} expect = False')
+print(f'{nums2} act = {sol.increasingTriplet(nums2)} expect = True')
+print(f'{nums3} act = {sol.increasingTriplet(nums3)} expect = True')
+print(f'{nums4} act = {sol.increasingTriplet(nums4)} expect = False')
+print(f'{nums5} act = {sol.increasingTriplet(nums5)} expect = True')
+print(f'{nums6} act = {sol.increasingTriplet(nums6)} expect = True')
+print(f'{nums7} act = {sol.increasingTriplet(nums7)} expect = False')
+print(f'{nums8} act = {sol.increasingTriplet(nums8)} expect = False')
+print(f'{nums9} act = {sol.increasingTriplet(nums9)} expect = True')
