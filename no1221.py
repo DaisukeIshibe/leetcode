be8b7b0dd64cc1b398495afcea7463fa609cc687
Daisuke Ishibe <daisuke.ishibe@gmail.com>
2025-09-15 11:43:18 +0900

    Update for no3271

--- no1221.py
+++ no1221.py
@@ -1,9 +1,21 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
         count: int = 0
-        count_rl = s.count('RL')
-        count_lr = s.count('LR')
-        return min(count_lr, count_rl)
+        sub_len = len(s) // 2
+        for i in range(sub_len, 0, -1):
+            sub_rl = 'R' * i + 'L' * i
+            sub_lr = 'L' * i + 'R' * i
+            c = s.count(sub_rl)
+            if c > 0:
+                count += c
+                s = s.replace(sub_rl, '|')
+                print(f'Match {sub_rl} -> {s}')
+            c = s.count(sub_lr)
+            if c > 0:
+                count += c
+                s = s.replace(sub_lr, '|')
+                print(f'Match {sub_lr} -> {s}')
+        return count
 
 sol = Solution()
 print(f'{sol.balancedStringSplit("RLRRLLRLRL")} expect 4')
--- no3271.py
+++ no3271.py
@@ -1,10 +1,22 @@
 class Solution:
     def stringHash(self, s: str, k: int) -> str:
-        for c in s:
-            a_c = ord(c) - 97
-            print(f'{c} {a_c}')
-        return ''
+        len_s = len(s)
+        idx = 0
+        out_str:str = ''
+        while idx < len_s:
+            sub_str = s[idx:idx + k]
+            sub_total = 0
+            for i in range(k):
+                c = sub_str[i]
+                sub_total += (ord(c) - 97)
+            s_ = chr((sub_total % 26) + 97)
+            idx += k
+            out_str += s_
+            #print(s, sub_str, s_)
+        return out_str
 
 sol = Solution()
 print(f'{sol.stringHash("abcd", 2)} expect bf')
 print(f'{sol.stringHash("mxz", 3)} expect i')
+print(f'{sol.stringHash("a", 1)} expect a')
+print(f'{sol.stringHash("ahauqh", 2)} expect hux')
