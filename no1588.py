350da30e48b72719d7790d3a65343ec8bb4cd587
Daisuke Ishibe <daisuke.ishibe@gmail.com>
2025-09-17 16:44:30 +0900

    Update for no1588

--- no1588.py
+++ no1588.py
@@ -1,4 +1,16 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: list[int]) -> int:
-        return 0
+        len_arr = len(arr)
+        len_list = [i for i in range(1, len_arr + 1) if i % 2 == 1]
+        total = 0
+        for len_ in len_list:
+            for i in range(0, len_arr - len_ + 1):
+                sub_arr = arr[i:i+len_]
+                total += sum(sub_arr)
+        return total
 
+
+sol = Solution()
+print(f'{sol.sumOddLengthSubarrays([1,4,2,5,3])} expect 58')
+print(f'{sol.sumOddLengthSubarrays([1,2])} expect 3')
+print(f'{sol.sumOddLengthSubarrays([10,11,12])} expect 66')
