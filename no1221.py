8e9ec72ca4420eb84d1b0e649ac7654688b07634
Daisuke Ishibe <daisuke.ishibe@gmail.com>
2025-10-04 12:38:07 +0900

    Updte for no143

--- no1221.py
+++ no1221.py
@@ -1,20 +1,15 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
         count: int = 0
-        sub_len = len(s) // 2
-        for i in range(sub_len, 0, -1):
-            sub_rl = 'R' * i + 'L' * i
-            sub_lr = 'L' * i + 'R' * i
-            c = s.count(sub_rl)
-            if c > 0:
-                count += c
-                s = s.replace(sub_rl, '|')
-                print(f'Match {sub_rl} -> {s}')
-            c = s.count(sub_lr)
-            if c > 0:
-                count += c
-                s = s.replace(sub_lr, '|')
-                print(f'Match {sub_lr} -> {s}')
+        dist_dict = {'L': 0, 'R': 0}
+        sub_str: str = ''
+        for c in s:
+            dist_dict[c] += 1
+            sub_str += c
+            if dist_dict['L'] == dist_dict['R']:
+                count += 1
+                dist_dict['L'] = 0
+                dist_dict['R'] = 0
         return count
 
 sol = Solution()
---
+++ no1309.py
@@ -0,0 +1,30 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        w_dict = {'1':'a', '2':'b', '3':'c', '4':'d', '5':'e', '6':'f', '7':'g', '8':'h', '9':'i',
+                  '10#':'j', '11#':'k', '12#':'l', '13#':'m', '14#':'n', '15#':'o', '16#':'p',
+                  '17#':'q', '18#':'r', '19#':'s','20#':'t','21#':'u','22#':'v','23#':'w',
+                  '24#':'x', '25#':'y','26#':'z'}
+
+        print(s)
+        out_list = []
+        skip_flag = False
+        for i in range(len(s) - 2):
+            a = s[i]
+            b = s[i + 1]
+            c = s[i + 2]
+            if c == '#':
+                skip_flag = True
+                sub_str = a + b + c
+                print('out->', w_dict[sub_str])
+            else:
+                if skip_flag:
+                    if a == '#':
+                        skip_flag = False
+                    continue
+                else:
+                    print(a,b,c)
+        return out_list
+
+sol = Solution()
+print(f'{sol.freqAlphabets("10#11#12")} expect jkab')
+print(f'{sol.freqAlphabets("1326#")} expect acz')
