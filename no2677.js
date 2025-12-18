8e350d4489ce6ea55ecc53266fb917e3ef4b04ec
Daisuke Ishibe <daisuke.ishibe@gmail.com>
2025-12-19 07:13:21 +0900

    Update for no3606

---
+++ no2677.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ * 
+Given an array arr and a chunk size size, return a chunked array.
+A chunked array contains the original elements in arr, but consists of subarrays each of length size. The length of the last subarray may be less than size if arr.length is not evenly divisible by size.
+Please solve it without using lodash's _.chunk function.
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], size = 1
+Output: [[1],[2],[3],[4],[5]]
+Explanation: The arr has been split into subarrays each with 1 element.
+Example 2:
+
+Input: arr = [1,9,6,3,2], size = 3
+Output: [[1,9,6],[3,2]]
+Explanation: The arr has been split into subarrays with 3 elements. However, only two elements are left for the 2nd subarray.
+ */
+var chunk = function(arr, size) {
+	const result = [];
+	for (let i = 0; i < arr.length; i += size) {
+		result.push(arr.slice(i, i + size));
+	}
+	return result;
+};
