bce3043bee6ea0b8935c6c8ddd8706081af273e3
Daisuke Ishibe <daisuke.ishibe@gmail.com>
2026-02-01 10:47:57 +0900

    Update for no563

---
+++ no563.py
@@ -0,0 +1,19 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        self.tilt = 0
+        def dfs(node):
+            if not node:
+                return 0
+            left_sum = dfs(node.left)
+            right_sum = dfs(node.right)
+            self.tilt += abs(left_sum - right_sum)
+            return left_sum + right_sum + node.val
+        dfs(root)
+        return self.tilt
