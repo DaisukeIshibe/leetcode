eeefee717fb70921f523ca394a5aa4fba60044cb
Daisuke Ishibe <daisuke.ishibe@gmail.com>
2026-02-04 17:39:50 +0900

    Update for no653

---
+++ no653.py
@@ -0,0 +1,25 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        seen = set()
+        
+        def dfs(node: Optional[TreeNode]) -> bool:
+            if not node:
+                return False
+            
+            complement = k - node.val
+            if complement in seen:
+                return True
+            
+            seen.add(node.val)
+            
+            return dfs(node.left) or dfs(node.right)
+        
+        return dfs(root)
